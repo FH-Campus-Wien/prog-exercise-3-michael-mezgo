5d435e6d68ec44bffdfc56254e5f86730a34ca92
michael-mezgo <58122000+michael-mezgo@users.noreply.github.com>
2022-10-14 14:09:06 +0200

    Completed Task 1

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Exercise 3
 
+## Classroom Status:
+[![GitHub Classroom Workflow](https://github.com/FH-Campus-Wien/prog-exercise-3-michael-mezgo/actions/workflows/classroom.yml/badge.svg)](https://github.com/FH-Campus-Wien/prog-exercise-3-michael-mezgo/actions/workflows/classroom.yml)
+
 > Please add your solutions to App.java under src/main/java/...
 
 > Methods, Arrays
@@ -7,7 +10,7 @@
 Please find a task description in the seperate handout on the moodle plattform of this course.
 
 Good luck :four_leaf_clover:
-- [ ] Task 1
+- :white_check_mark: Task 1
 - [ ] Task 2
 - [ ] Task 3
 - [ ] Task 4
--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,8 +1,5 @@
 package at.ac.fhcampuswien;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class App {
 
     // Implement all methods as public static
@@ -13,4 +10,58 @@ public class App {
         // print their results
         // etc.
     }
+
+    public static void oneMonthCalendar(int daysInMonth, int monthBeginDay)
+    {
+        int currentDay = 1;
+        final int weekLength = 7;
+        int currentWeekLength = weekLength - monthBeginDay + 1;
+
+        System.out.print(generateBeginMonthWhiteSpace(monthBeginDay));
+
+        do
+        {
+            System.out.println(generateWeekRow(currentDay, currentWeekLength, daysInMonth));
+            currentDay += currentWeekLength;
+            currentWeekLength = weekLength;
+        }
+        while (currentDay <= daysInMonth);
+    }
+
+    //region Private functions Task 1
+    private static String generateBeginMonthWhiteSpace (int monthBeginDay)
+    {
+        StringBuilder output = new StringBuilder();
+        String threeSpaces = "   ";
+        for (int i = 0; i < monthBeginDay - 1; i++) {
+            output.append(threeSpaces);
+        }
+
+        return output.toString();
+    }
+
+    private static String generateWeekRow (int firstDayInRow, int weekLength, int daysInMonth)
+    {
+        int currentDay = firstDayInRow;
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < weekLength; i++) {
+            if(currentDay > daysInMonth)
+            {
+                return output.toString();
+            }
+            if (currentDay < 10)
+            {
+                output.append(" ").append(currentDay).append(" ");
+            }
+            else
+            {
+                output.append(currentDay).append(" ");
+            }
+            currentDay ++;
+        }
+
+        return output.toString();
+    }
+    //endregion
 }
